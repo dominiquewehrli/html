a37133c31b60a6aa13bc2d5ac7de18f6818be185
dominiquewehrli <mail@dominiquewehrli.ch>
2024-11-27 22:00:38 +0100

    Update s.js

--- s.js
+++ s.js
@@ -1,11 +1,11 @@
 
 document.onkeydown = function (e) {
   if (!e) e = window.event;
-  if (e.key == '1') { document.location = "/sr" }
-  if (e.key == '2') { document.location = "/rb" }
-  if (e.key == '3') { document.location = "/es" }
-  if (e.key == '4') { document.location = "/bg" }
-  if (e.key == '5') { document.location = "/map" }
-  if (e.key == '6') { document.location = "/osm" }
-  if (e.key == '7') { document.location = "/in" }
+  if (e.key == '1') { window.open("/sr") }
+  if (e.key == '2') { window.open("/rb") }
+  if (e.key == '3') { window.open("/es") }
+  if (e.key == '4') { window.open("/bg") }
+  if (e.key == '5') { window.open("/map") }
+  if (e.key == '6') { window.open("/osm") }
+  if (e.key == '7') { window.open("/in") }
 }
