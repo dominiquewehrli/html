db2e5de0cf6fcc64ba3d8cfbda815bb2e6b32816
dominiquewehrli <mail@dominiquewehrli.ch>
2024-11-28 18:01:50 +0100

    Update s.js

--- s.js
+++ s.js
@@ -13,7 +13,8 @@ document.onkeydown = function (e) {
 }
 
 function sr () {
-  var q = prompt('Bundesrecht: \nErlass Artikel (z.B. BV 75) oder Erlassnummer Artikel (z.B. 101 75).').toUpperCase().replace(/ +$/, ""); if (q) { document.location = "https://www.fedlex.admin.ch/de/search?collection=classified_compilation&classifiedBy=" + q }
+  var q = prompt('Bundesrecht: \nErlass Artikel (z.B. BV 75) oder Erlassnummer Artikel (z.B. 101 75).').toUpperCase().replace(/ +$/, ""); 
+  if (q) { document.location = "https://www.fedlex.admin.ch/de/search?collection=classified_compilation&classifiedBy=" + q }
 }
 
 function rb () {
@@ -25,15 +26,18 @@ function rb () {
     q = q.replace("tg waldg", "921.1").replace("tg waldv", "921.11").replace("waldg", "921.1").replace("waldv", "921.11");
     q = q.replace("wbsng", "721.1").replace("wbg", "721.1").replace("wng", "721.8").replace("wbsnv", "721.11").replace("wbv", "721.11");
     q = q.replace("strwg", "725.1").replace("strwv", "725.10").replace("göb", "720.1").replace("ivöb", "720.3").replace("fhg", "611.1");
-    q = q.replace(" ", "/art/"); document.location = "https://www.rechtsbuch.tg.ch/app/de/texts_of_law/" + q }
+    q = q.replace(" ", "/art/"); 
+    if (q) { document.location = "https://www.rechtsbuch.tg.ch/app/de/texts_of_law/" + q } }
 }
 
 function es () {
-  var q = prompt('Entscheidsuche: \nErlass Artikel (z.B. BGB 75).').replace(/([0-9]) ([0-9])/, "$1~$2").replace(/ +$/, "").replace(/([A-Za-z]+) ([0-9a-z]+)/, "\"Art. $2\" AND \"$2 $1\"~5").replace(/([0-9])~([0-9])/, "$2"); document.location = "https://entscheidsuche.ch/search?query=" + q
+  var q = prompt('Entscheidsuche: \nErlass Artikel (z.B. BGB 75).').replace(/([0-9]) ([0-9])/, "$1~$2").replace(/ +$/, "").replace(/([A-Za-z]+) ([0-9a-z]+)/, "\"Art. $2\" AND \"$2 $1\"~5").replace(/([0-9])~([0-9])/, "$2"); 
+  if (q) { document.location = "https://entscheidsuche.ch/search?query=" + q }
 }
 
 function bg () {
-  var q = prompt('Bundesgericht: \nUrteil des BGer (z.B. 1C_483/2021).').replace(" ", "_").replace(" ", "/"); window.location.href = "https://www.bger.ch/ext/eurospider/live/de/php/aza/http/index.php?lang=de&type=simple_query&query_words=" + q;
+  var q = prompt('Bundesgericht: \nUrteil des BGer (z.B. 1C_483/2021).').replace(" ", "_").replace(" ", "/"); 
+  if (q) { document.location = "https://www.bger.ch/ext/eurospider/live/de/php/aza/http/index.php?lang=de&type=simple_query&query_words=" + q }
 }
 
 function m () {
@@ -41,7 +45,8 @@ function m () {
 }
 
 function osm () {
-  var q = prompt('Open Street Map:').replace(/ +$/, ""); if (q) { document.location = "https://www.openstreetmap.org/search?query=" + q }
+  var q = prompt('Open Street Map:').replace(/ +$/, ""); 
+  if (q) { document.location = "https://www.openstreetmap.org/search?query=" + q }
 }
 
 function i () {
