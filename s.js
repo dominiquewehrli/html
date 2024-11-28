e22867c2408e9e7bf953899792e47dd0d3d5827a
dominiquewehrli <mail@dominiquewehrli.ch>
2024-11-28 06:47:09 +0100

    Update s.js

--- s.js
+++ s.js
@@ -1,14 +1,6 @@
 
 document.onkeydown = function (e) {
   if (!e) e = window.event;
-  if (e.key == '1') { window.open("/sr") }
-  if (e.key == '2') { window.open("/rb") }
-  if (e.key == '3') { window.open("/es") }
-  if (e.key == '4') { window.open("/bg") }
-  if (e.key == '5') { window.open("/map") }
-  if (e.key == '6') { window.open("/osm") }
-  if (e.key == '7') { window.open("/in") }
-
   if (e.key == 's') { window.open("/sr") }
   if (e.key == 'r') { window.open("/rb") }
   if (e.key == 'e') { window.open("/es") }
@@ -16,4 +8,5 @@ document.onkeydown = function (e) {
   if (e.key == 'm') { window.open("/map") }
   if (e.key == 'o') { window.open("/osm") }
   if (e.key == 'i') { window.open("/in") }
+  if (e.key == 'k') { window.open("/kollektion") }
 }
