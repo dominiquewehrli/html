573896a1692fdcada5b263d1807088ee9d0cc8f8
dominiquewehrli <mail@dominiquewehrli.ch>
2024-11-28 07:28:57 +0100

    Update s.js

--- s.js
+++ s.js
@@ -1,7 +1,9 @@
 s = ""
 document.onkeydown = function (e) {
   if (!e) e = window.event;
-  if (e.key == 's') { if (s == "") { s = "/sr" } }
+  // if (e.key == 's') { if (s == "") { s = "/sr" } }
+  if (e.key == 's') { if (s == "") { var q = prompt('Erlass Artikel (z.B. BV 75) oder Erlassnummer Artikel (z.B. 101 75).').toUpperCase().replace(/ +$/, "");
+document.location = "https://www.fedlex.admin.ch/de/search?collection=classified_compilation&classifiedBy=" + q }
   if (e.key == 'r') { if (s == "") { s = "/rb" } }
   if (e.key == 'e') { if (s == "") { s = "/es" } }
   if (e.key == 'b') { if (s == "") { s = "/bg" } }
