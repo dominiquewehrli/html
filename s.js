393778ead6c4997f13b50060faf8db8786eadc66
dominiquewehrli <mail@dominiquewehrli.ch>
2024-11-28 07:02:39 +0100

    Update s.js

--- s.js
+++ s.js
@@ -1,13 +1,13 @@
 s = ""
 document.onkeydown = function (e) {
   if (!e) e = window.event;
-  if (e.key == 's') { s = "/sr" }
-  if (e.key == 'r') { s = "/rb" }
-  if (e.key == 'e') { s = "/es" }
-  if (e.key == 'b') { s = "/bg" }
-  if (e.key == 'm') { s = "/map" }
-  if (e.key == 'o') { s = "/osm" }
-  if (e.key == 'i') { s = "/in" }
-  if (e.key == 'k') { s = "/kollektion" }
+  if (e.key == 's') { if (s == "") { s = "/sr" } }
+  if (e.key == 'r') { if (s == "") { s = "/rb" } }
+  if (e.key == 'e') { if (s == "") { s = "/es" } }
+  if (e.key == 'b') { if (s == "") { s = "/bg" } }
+  if (e.key == 'm') { if (s == "") { s = "/map" } }
+  if (e.key == 'o') { if (s == "") { s = "/osm" } }
+  if (e.key == 'i') { if (s == "") { s = "/in" } }
+  if (e.key == 'k') { if (s == "") { s = "/kollektion" } }
   if (e.key == 'Enter') { t = s; s = ""; document.location = t }
 }
