829266087ee50d77e6bfcba4cef3a5583d14978f
dominiquewehrli <mail@dominiquewehrli.ch>
2024-11-28 17:19:51 +0100

    Update s.js

--- s.js
+++ s.js
@@ -2,21 +2,21 @@ echts = ""
 document.onkeydown = function (e) {
   if (!e) e = window.event;
   // if (e.key == 's') { if (s == "") { s = "/sr" } }
-  if (e.key == 's') { s() }
-  if (e.key == 'r') { r() }
+  if (e.key == 's') { sr() }
+  if (e.key == 'r') { rb() }
   if (e.key == 'e') { es() }
-  if (e.key == 'b') { b() }
-  if (e.key == 'm') { m() }
-  if (e.key == 'o') { o() }
+  if (e.key == 'b') { bg() }
+  if (e.key == 'm') { map() }
+  if (e.key == 'o') { osm() }
   if (e.key == 'i') { i() }
   if (e.key == 'k') { k() }
 }
 
-function s () {
+function sr () {
   var q = prompt('Bundesrecht: \nErlass Artikel (z.B. BV 75) oder Erlassnummer Artikel (z.B. 101 75).').toUpperCase().replace(/ +$/, ""); document.location = "https://www.fedlex.admin.ch/de/search?collection=classified_compilation&classifiedBy=" + q
 }
 
-function r () {
+function rb () {
   var q = prompt('Rechtsbuch TG: \nErlass Artikel (z.B. PBG 17) oder Erlassnummer Artikel (z.B. 700 17). Erlassbezeichnungen werden fortlaufend ergaenzt.').toLowerCase().replace(/ +$/, ""); if (q == "") { document.location = "https://www.rechtsbuch.tg.ch/app/de/systematic/search" } else {
     q = q.replace("kv", "101").replace("gemg", "131.1").replace("eg zgb", "210.1").replace("archivg", "432.10").replace("eng", "731.1").replace("tg dsg", "170.7").replace("dsg", "170.7").replace("rsv", "177.112");
     q = q.replace("vrg", "170.1").replace("öffg", "170.6").replace("dsv", "170.71").replace("vgv","631.1");
@@ -32,15 +32,15 @@ function es () {
   var q = prompt('Entscheidsuche: \nErlass Artikel (z.B. BGB 75).').replace(/([0-9]) ([0-9])/, "$1~$2").replace(/ +$/, "").replace(/([A-Za-z]+) ([0-9a-z]+)/, "\"Art. $2\" AND \"$2 $1\"~5").replace(/([0-9])~([0-9])/, "$2"); document.location = "https://entscheidsuche.ch/search?query=" + q
 }
 
-function b () {
+function bg () {
   var q = prompt('Bundesgericht: \nUrteil des BGer (z.B. 1C_483/2021).').replace(" ", "_").replace(" ", "/"); window.location.href = "https://www.bger.ch/ext/eurospider/live/de/php/aza/http/index.php?lang=de&type=simple_query&query_words=" + q;
 }
 
-function m () {
+function map () {
   document.location = "https://s.geo.admin.ch/unoha5jgibb1"
 }
 
-function o () {
+function osm () {
   var q = prompt('Open Street Map:').replace(/ +$/, ""); if (q) { document.location = "https://www.openstreetmap.org/search?query=" + q }
 }
 
