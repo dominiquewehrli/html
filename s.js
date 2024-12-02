e4db44b14cd74a39658b42f634d64b9437e36307
dominiquewehrli <mail@dominiquewehrli.ch>
2024-12-02 09:48:35 +0100

    Update s.js

--- s.js
+++ s.js
@@ -31,7 +31,7 @@ function rb () {
 }
 
 function es () {
-  var q = prompt('Entscheidsuche: \nErlass Artikel (z.B. BGB 75).').toUpperCase().replace(" V", " Abs. 5").replace(" IV", " Abs. 4").replace(" III", " Abs. 3").replace(" II", " Abs. 2").replace(" I", " Abs. 1").replace(/([0-9]) ([0-9])/, "$1~$2").replace(/ +$/, "").replace(/([A-Za-z]+) ([0-9a-z]+)( Abs. [0-9])?/, "\"Art. $2\" AND \"$2$3 $1\"~5").replace(/([0-9])~([0-9])/, "$2"); 
+  var q = prompt('Entscheidsuche: \nErlass Artikel (z.B. BGB 75).').toUpperCase().replace(" V", " Abs. 5").replace(" IV", " Abs. 4").replace(" III", " Abs. 3").replace(" II", " Abs. 2").replace(" I", " Abs. 1").replace(/([0-9]) ([0-9])/, "$1~$2").replace(/ +$/, "").replace(/([A-Za-z]+) ([0-9a-z]+)( Abs. [0-9])?/, "\"Art. $2\" AND \"$2$3\" \"$2 $1\"~5").replace(/([0-9])~([0-9])/, "$2"); 
   if (q) { document.location = "https://entscheidsuche.ch/search?query=" + q }
 }
 
