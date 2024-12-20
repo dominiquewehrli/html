56dc892bcc79525d14ccce3fddf3a302e83c5bbc
dominiquewehrli <mail@dominiquewehrli.ch>
2024-12-20 10:34:28 +0100

    Create k.js

---
+++ k.js
@@ -0,0 +1,13 @@
+document.onkeydown = function (e) {
+  if (!e) e = window.event;
+  if (e.key == 's') { sr() }
+  if (e.key == 'r') { rb() }
+  if (e.key == 'e') { es() }
+  if (e.key == 'b') { bg() }
+  if (e.key == 'm') { map() }
+  if (e.key == 'o') { osm() }
+  if (e.key == 'i') { lin() }
+  if (e.key == 't') { st() }
+  if (e.key == 'k') { kollektion() }
+  if (e.key == ' ') { edit() }
+}
