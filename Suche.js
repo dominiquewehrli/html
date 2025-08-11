7429f3b4ba956867c869ea3f08f4c7f23b5acd68
dominiquewehrli <mail@dominiquewehrli.ch>
2025-08-11 14:31:35 +0200

    Update Suche.js

--- Suche.js
+++ Suche.js
@@ -9,7 +9,7 @@ function rb () {
   q = q.replace("tg dsg", "170.7").replace("dsg", "170.7").replace("rsv", "177.112").replace("geoig", "211.441").replace("geoiv", "211.442");
   q = q.replace("vrg", "170.1").replace("öffg", "170.6").replace("dsv", "170.71").replace("rrv vgv","631.11").replace("vgv","631.1").replace("tg entg","710").replace("entg","710");
   q = q.replace("pbg", "700").replace("pbv", "700.1").replace("ivhb", "700.2").replace("ksv", "700.3").replace("flgg", "913.1").replace("flurg", "913.1");
-  q = q.replace("tg nhg", "450.1").replace("tg nhv", "450.11").replace("nhg", "450.1").replace("nhv", "450.11").replace("usgv", "814.03");
+  q = q.replace("tg nhg", "450.1").replace("tg nhv", "450.11").replace("nhg", "450.1").replace("nhv", "450.11").replace("usgv", "814.03").replace("abfallg", "814.04");
   q = q.replace("tg waldg", "921.1").replace("tg waldv", "921.11").replace("waldg", "921.1").replace("waldv", "921.11").replace("tg vlv", "170.21").replace("vlv", "170.21");
   q = q.replace("wbsng", "721.1").replace("wbg", "721.1").replace("wng", "721.8").replace("wnv", "721.81").replace("wbsnv", "721.11").replace("wbv", "721.11");
   q = q.replace("strwg", "725.1").replace("strwv", "725.10").replace("göb", "720.1").replace("ivöb", "720.3").replace("fhg", "611.1");
